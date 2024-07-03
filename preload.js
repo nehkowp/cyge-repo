c5eebc0b9bec8f8ccf84aa6a7cf60230fd914558
kowpaul <brechenmac@cy-tech.fr>
2024-07-03 11:28:40 +0200

    signIn & signUp rework

--- preload.js
+++ preload.js
@@ -1,6 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  submitLogin: (login, password) => ipcRenderer.send('submit-login', { login, password }),
-  onLoginResponse: (callback) => ipcRenderer.on('login-response', callback)
+  submitSignIn: (login, password) => ipcRenderer.send('submit-login', { login, password }),
+  onLoginResponse: (callback) => ipcRenderer.on('login-response', callback),
+  submitSignUp: (nom, prenom, login, password, lang) => ipcRenderer.send('submit-signup', { nom, prenom, login, password, lang }),
+  onSignUpResponse: (callback) => ipcRenderer.on('signup-response', callback)
 });
--- src/login.js
+++ src/login.js
@@ -21,3 +21,19 @@ const loginForm = document.getElementById('signInForm');
     window.electronAPI.onLoginResponse((event, response) => {
       alert(response.message);
     });
+
+    const signUpForm = document.getElementById('signUpForm');
+    signUpForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const nom = document.getElementById('nom').value;
+      const prenom = document.getElementById('prenom').value;
+      const login = document.getElementById('login_su').value;
+      const password = document.getElementById('password_su').value;
+      const lang = document.getElementById('lang').value;
+      window.electronAPI.submitSignUp(nom, prenom, login, password, lang);
+    });
+
+    window.electronAPI.onSignUpResponse((event, response) => {
+      alert(response.message);
+    });
+    
